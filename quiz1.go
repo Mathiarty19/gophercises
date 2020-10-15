eeed6b2136fdf3c90add88de2c3b2d6b263dc08d
Mathiarty19 <jacobsoncharlie@gmail.com>
2020-10-15 00:26:25 -0500

    quiz and timer

--- quiz1.go
+++ quiz1.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"time"
 	"encoding/csv"
 	"fmt"
 	"io"
@@ -10,39 +11,58 @@ import (
 
 func main() {
 	q := csvOpen()
-	a := getAnswer()
-	quiz(q, a)
+	// a := getAnswer(q)
+	// quizResults(q, a)
+	getAnswer(q)
 
 }
+type Test struct {
+	question string
+	answer string
+}
 
-func csvOpen() []string {
+func csvOpen() []*Test {
 	f, err := os.Open(os.Args[1])
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 	r := csv.NewReader(f)
-	var record []string
+	questions := make([]*Test, 12)
+	var i int = 0
 	for {
-		record, err = r.Read()
+		record, err := r.Read()
 		if err == io.EOF {
 			break
 		}
 		if err != nil {
 			log.Fatal(err)
 		}
-
-		fmt.Println(record)
+		questions[i] = &Test{question: record[0], answer: record[1]}
+		i++
 	}
-	return record
+	// fmt.Println(questions)
+	return questions
 }
 
-func quiz(q []string, a string) bool {
-	// if answer and their given answer match return true ect
-}
+// func quizResults(q []string, a string) bool {
+// 	// if answer and their given answer match return true ect
+// }
 
-func getAnswer() string {
-
-	fmt.Println(resp)
-	return resp
+func getAnswer(questions []*Test) []string  {
+	answers := make([]string, 12)
+	fmt.Println("Test starting you have thirty seconds")
+	start := time.Now()
+	for i := 0; i < 12; i++{
+		var answer string
+		if time.Since(start) > time.Second * 30 {
+			fmt.Println("Times Up!")
+			break
+		} else {
+		fmt.Println(questions[i].question)
+		fmt.Scanf("%s", &answer)
+		answers = append(answers, answer)
+		}
+	}
+	return answers
 }
